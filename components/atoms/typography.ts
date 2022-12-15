fb8f1a15abfd5cc6e332cf85d6f8eb166504707e
Balazs Radvanyi <balazs@riltech.co>
2022-12-15 16:57:03 +0100

    feat(atoms): added typography and colors

---
+++ components/atoms/typography.ts
@@ -0,0 +1,66 @@
+import styled from 'styled-components';
+import { Grey010 } from './colors';
+
+export const PageTitle = styled.h1`
+        font-family: 'Figtree', sans-serif;
+        font-style: normal;
+        font-weight: 600;
+        font-size: 64px;
+        line-height: 120%;
+        color: ${Grey010};
+    `;
+
+export const H1 = styled.h1`
+    font-family: 'Gentium Book Basic', serif;
+    font-style: normal;
+    font-weight: 700;
+    font-size: 48px;
+    line-height: 120%;
+    color: ${Grey010};
+`;
+
+export const H2 = styled.h2`
+    font-family: 'Gentium Book Basic', serif;
+    font-style: normal;
+    font-weight: 700;
+    font-size: 36px;
+    line-height: 120%;
+    color: ${Grey010};
+`;
+
+export const H3 = styled.h3`
+    font-family: 'Figtree', sans-serif;
+    font-style: normal;
+    font-weight: 700;
+    font-size: 24px;
+    line-height: 120%;
+    color: ${Grey010};
+`;
+
+export const LargeParagraph = styled.p`
+    font-family: 'Gentium Book Basic', serif;
+    font-style: normal;
+    font-weight: 400;
+    font-size: 20px;
+    line-height: 140%;
+    color: ${Grey010};
+`;
+
+export const DefaultParagraph = styled.p`
+    font-family: 'Figtree', sans-serif;
+    font-style: normal;
+    font-weight: 400;
+    font-size: 16px;
+    line-height: 140%;
+    color: ${Grey010};
+`;
+
+export const SmallParagraph = styled.p`
+    font-family: 'Figtree', sans-serif;
+    font-style: normal;
+    font-weight: 400;
+    font-size: 12px;
+    line-height: 140%;
+    color: ${Grey010};
+`;
+
