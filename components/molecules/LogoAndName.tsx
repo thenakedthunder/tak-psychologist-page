340ba9c7a33a62267144cb7712012c06aa7b746b
Balazs Radvanyi <balazs@riltech.co>
2023-01-10 16:56:09 +0100

    refactor(navbar): restructured mobile navbar

---
+++ components/molecules/LogoAndName.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import styled from 'styled-components';
+
+import { IoIosRose } from 'react-icons/io';
+
+import { Grey010 } from 'components/styling/colors';
+import { DefaultParagraph } from 'components/atoms/typography.styles';
+
+const MobileLeftDiv = styled.div`
+    background-color: ${Grey010};
+    display: grid;
+    grid-template-columns: 36px 1fr; 
+`;
+
+const LogoDiv = styled.div`
+    padding: 8px 8px 8px 0;
+`;
+
+const NameParagraph = styled(DefaultParagraph)`
+    font-weight: 700;
+`;
+
+const LogoAndName = () => (
+  <MobileLeftDiv>
+    <LogoDiv><IoIosRose size={28} /></LogoDiv>
+    <div>
+      <div>
+        <NameParagraph>Hont-Gáspár Anita</NameParagraph>
+      </div>
+      <div>
+        <DefaultParagraph>Pszichológus</DefaultParagraph>
+      </div>
+    </div>
+  </MobileLeftDiv>
+);
+
+export default LogoAndName;
--- components/organisms/NavBar.tsx
+++ components/organisms/NavBar.tsx
@@ -1,34 +1,16 @@
 import styled from 'styled-components';
-import { IoIosRose, IoIosMenu } from 'react-icons/io';
+import { IoIosMenu } from 'react-icons/io';
 
 import { Grey010 } from 'components/styling/colors';
-import { DefaultParagraph } from 'components/atoms/typography.styles';
+import LogoAndName from 'components/molecules/LogoAndName';
 
 const NavBarMobile = styled.div`
     box-sizing: border-box;
     height: 84px;
     background-color: ${Grey010};
+    padding: 20px;
     display: grid;
     grid-template-columns: 1fr 1fr; 
-    padding: 20px;
-`;
-
-const InnerNavBarMobileDiv = styled.div`
-    padding: 8px;
-`;
-
-const MobileLeftDiv = styled.div`
-    background-color: ${Grey010};
-    display: grid;
-    grid-template-columns: 36px 1fr; 
-`;
-
-const LogoDiv = styled.div`
-    padding: 8px 8px 8px 0;
-`;
-
-const NameParagraph = styled(DefaultParagraph)`
-    font-weight: 700;
 `;
 
 const MobileRightDiv = styled.div`
@@ -37,24 +19,12 @@ const MobileRightDiv = styled.div`
 `;
 
 const NavBar = () => (
-  <InnerNavBarMobileDiv>
-    <NavBarMobile>
-      <MobileLeftDiv>
-        <LogoDiv><IoIosRose size={28} /></LogoDiv>
-        <div>
-          <div>
-            <NameParagraph>Hont-Gáspár Anita</NameParagraph>
-          </div>
-          <div>
-            <DefaultParagraph>Pszichológus</DefaultParagraph>
-          </div>
-        </div>
-      </MobileLeftDiv>
-      <MobileRightDiv>
-        <IoIosMenu size={28} />
-      </MobileRightDiv>
-    </NavBarMobile>
-  </InnerNavBarMobileDiv>
+  <NavBarMobile>
+    <LogoAndName />
+    <MobileRightDiv>
+      <IoIosMenu size={28} />
+    </MobileRightDiv>
+  </NavBarMobile>
 );
 
 export default NavBar;
