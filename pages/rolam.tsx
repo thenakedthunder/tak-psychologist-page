085f3c02e91d4787062118bcfa2aad5721031249
Balazs Radvanyi <balazs@riltech.co>
2023-02-15 15:33:45 +0100

    fix(design): added background color to page

--- pages/rolam.tsx
+++ pages/rolam.tsx
@@ -5,10 +5,12 @@ import NavBar from 'components/NavBar/organisms/NavBar';
 import Prose from 'components/AboutMe/organisms/Prose';
 import List from 'components/AboutMe/organisms/List';
 import TopGrid from 'components/AboutMe/organisms/TopGrid';
+import { Grey010 } from 'components/styling/colors';
 
 const MainContainer = styled.div`
   padding: 40px;
   max-width: 1440px;
+  background-color: ${Grey010};
 
   @media screen and (min-width: 1200px) {
     background-image: url('/assets/box-profile.svg');
