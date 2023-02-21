b00dd9454fc62d85aa1b55f78d7f42f8da40f5ac
Balazs Radvanyi <balazs@riltech.co>
2023-02-21 11:14:40 +0100

    wip(vercel): debugging vercel build

--- components/AboutMe/molecules/PictureOnShapesDesktop.tsx
+++ components/AboutMe/molecules/PictureOnShapesDesktop.tsx
@@ -1,7 +1,7 @@
 import styled from 'styled-components';
 import Image from 'next/image';
 
-import ProfileImage from '@/public/assets/rolam.png';
+// import ProfileImage from '@/public/assets/rolam.png';
 
 export const MainContainer = styled.div`
   display: none;
@@ -37,7 +37,7 @@ export const ProfileImageContainer = styled.div`
 const PictureOnShapesDesktop = () => (
   <MainContainer>
     <ProfileImageContainer>
-      <Image src={ProfileImage} objectFit="contain" />
+      {/* <Image src={ProfileImage} objectFit="contain" /> */}
     </ProfileImageContainer>
   </MainContainer>
 );
