d8374b06949ea9b0aeccb3a67902b674d646c99a
Balazs Radvanyi <balazs@riltech.co>
2023-02-28 15:13:49 +0100

    refactor(contents): put contents into a separated file

---
+++ components/Contact/content/contactContent.ts
@@ -0,0 +1,15 @@
+export const ReservationInfo = {
+  highlightedParagraphs: [
+    'Lórum ipse azonban nem kurrog házatot. A kérülő sütköző delmetelőket az érsejk nem a turat által komszerű fázásokkal megegyezően rugosodták fel tatás között.',
+  ],
+  listedParagraphs: [
+    'A rányos golság zetlete esetenként répiákkal is csibolt. „a fónához rányos hősi posztásukat” a szalt donziságoknak önállóan, más fogalás gödése nélkül kellett csinasodniuk. A koma és a tezelősség hősi posztás csak érsejk és öszvényök között volt fackás, a szalt mazatok tatás között nem tűnhettek.',
+    'Ennek az a borosa, hogy a koma közös matkát saltol a koli és a csendő között, mivel a küleledés repegeteli a minset azzal, hogy pecélre hunkad panságot. A győző „lácska” a rotmásak számára rabolyogta, hogy orolják a csapósakat, így a remuncban hatásosan színézett a venítő bardasara.',
+  ],
+};
+
+export const FooterInfo = {
+  address: '1114 Budapest, Bartók Béla út 12/2, 4em. 13.',
+  email: 'Rendelo@mail.hu',
+  phone: '(+36) 30 123 4567',
+};
--- pages/elerhetosegek.tsx
+++ pages/elerhetosegek.tsx
@@ -9,6 +9,7 @@ import {
 import NavBar from 'components/NavBar/organisms/NavBar';
 import ImageSlider from 'components/Contact/molecules/ImageSlider';
 import PsychoLink from 'components/atoms/PsychoLink';
+import { ReservationInfo, FooterInfo } from 'components/Contact/content/contactContent';
 
 const MainContainer = styled.div`
   padding: 40px;
@@ -23,11 +24,15 @@ const SubHeader = styled(LargeParagraph)`
   padding-bottom: 20px;
 `;
 
-const BulletedInfo = styled.div`
+const BulletedInfoContainer = styled.div`
   padding: 30px 0 40px;
   display: grid;
-  grid-template-columns: 16px auto;
   row-gap: 30px;
+`;
+
+const BulletedInfo = styled.div`
+  display: grid;
+  grid-template-columns: 16px auto;
   column-gap: 16px;
 `;
 
@@ -40,7 +45,7 @@ const BulletPoint = styled.div`
 `;
 
 const Location = styled.div`
-  padding: 16px 0 30px;
+  padding: 16px 0 20px;
 
   a {
     text-decoration: underline
@@ -67,26 +72,21 @@ const Contact = () => (
       <SubHeader color={Green050}>
         Bejelentkezési információk
       </SubHeader>
-      <DefaultParagraph color={Green050}>
-        Lórum ipse azonban nem kurrog házatot. A kérülő sütköző delmetelőket az érsejk nem a turat
-        által komszerű fázásokkal megegyezően rugosodták fel tatás között.
-      </DefaultParagraph>
-      <BulletedInfo>
-        <BulletPoint />
-        <SmallParagraph color={Green050}>
-          A rányos golság zetlete esetenként répiákkal is csibolt. „a fónához rányos hősi
-          posztásukat” a szalt donziságoknak önállóan, más fogalás gödése nélkül kellett
-          csinasodniuk. A koma és a tezelősség hősi posztás csak érsejk és öszvényök között volt
-          fackás, a szalt mazatok tatás között nem tűnhettek.
-        </SmallParagraph>
-        <BulletPoint />
-        <SmallParagraph color={Green050}>
-          Ennek az a borosa, hogy a koma közös matkát saltol a koli és a csendő között, mivel a
-          küleledés repegeteli a minset azzal, hogy pecélre hunkad panságot. A győző „lácska” a
-          rotmásak számára rabolyogta, hogy orolják a csapósakat, így a remuncban hatásosan
-          színézett a venítő bardasara.
-        </SmallParagraph>
-      </BulletedInfo>
+      {ReservationInfo.highlightedParagraphs.map((item, index) => (
+        <DefaultParagraph key={index} color={Green050}>
+          {item}
+        </DefaultParagraph>
+      ))}
+      <BulletedInfoContainer>
+        {ReservationInfo.listedParagraphs.map((item, index) => (
+          <BulletedInfo key={index}>
+            <BulletPoint />
+            <SmallParagraph color={Green050}>
+              {item}
+            </SmallParagraph>
+          </BulletedInfo>
+        ))}
+      </BulletedInfoContainer>
       <SubHeader color={Green050}>
         A konzultációk helyszíne
       </SubHeader>
@@ -94,7 +94,7 @@ const Contact = () => (
       <Location>
         <BolderText color={Green050}>Szervezet neve</BolderText>
         <SmallParagraph color={Green050}>
-          1114 Budapest, Bartók Béla út 12/2, 4em. 13.
+          {FooterInfo.address}
         </SmallParagraph>
         <SmallParagraph color={Green050}>
           <PsychoLink href="https://www.google.com/maps/place/Budapest,+Bart%C3%B3k+B%C3%A9la+%C3%BAt+12-em.+13,+1111/@47.4822872,19.0529605,17z/data=!4m5!3m4!1s0x4741dc4d9518da53:0x6bccd46420204e21!8m2!3d47.4822872!4d19.0529605">
@@ -106,11 +106,11 @@ const Contact = () => (
         <BolderText color={Green050}>Elérhetőség</BolderText>
         <SmallParagraph color={Green050}>
           <PsychoLink href="mailto:Rendelo@mail.hu">
-            Rendelo@mail.hu
+            {FooterInfo.email}
           </PsychoLink>
         </SmallParagraph>
         <SmallParagraph color={Green050}>
-          (+36) 30 123 4567
+          {FooterInfo.phone}
         </SmallParagraph>
       </ContactDetails>
     </MainContainer>
