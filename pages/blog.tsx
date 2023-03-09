dc7884253b49aade5661b9539af1dd126781f844
Balazs Radvanyi <balazs@riltech.co>
2023-03-09 12:52:00 +0100

    feat(hero-section): made top of page responsive

--- pages/blog.tsx
+++ pages/blog.tsx
@@ -2,39 +2,43 @@ import styled from 'styled-components';
 import Image from 'next/image';
 
 import NavBar from 'components/NavBar/organisms/NavBar';
-import { Green010, Grey010 } from 'components/styling/colors';
+import { Green010, Green050, Grey010 } from 'components/styling/colors';
 import HeroImage from '@/public/assets/article-header-image.png';
-
-const TopStripe = styled.div`
-  background-color: ${Green010};
-  height: 156px;
-
-  @media screen and (min-width: 768px) {
-    height: 320px;
-  }
-`;
-
-const ImageWrapper = styled.div`
-  position: relative;
-  top: -76px;
-
-  @media screen and (min-width: 768px) {
-    top: -240px;
-  }
-`;
+import { H2 } from 'components/atoms/typography.styles';
 
 const MainContainer = styled.div`
   background-color: ${Grey010};
-  padding: 0 20px;
+  padding: 26px 40px 30px;
+`;
+
+const TopSection = styled.div`
+  position: relative;
+`;
+
+const BackgroundStripe = styled.div`
+  height: 50%;
+  width: 100%;
+  background-color: ${Green010};
+  position: absolute;
+  top: 0;
+  left: 0;
+`;
+
+const ImageWrapper = styled.div`
+  padding: 0px 20px;
 `;
 
 const Blog = () => (
   <>
-    <TopStripe>
-      <NavBar backgroundColor={Green010} />
-    </TopStripe>
+    <NavBar backgroundColor={Green010} />
+    <TopSection>
+      <BackgroundStripe />
+      <ImageWrapper>
+        <Image src={HeroImage} />
+      </ImageWrapper>
+    </TopSection>
     <MainContainer>
-      <ImageWrapper><Image src={HeroImage} /></ImageWrapper>
+      <H2 color={Green050}>Bejegyzés címe, maximum 2-3 sorban, lorem ipsum dolor sitamet</H2>
     </MainContainer>
   </>
 );
