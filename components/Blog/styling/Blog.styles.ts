6f88c6d636df8df8da20443c7d88cbe046bfb27c
Balazs Radvanyi <balazs@riltech.co>
2023-03-28 16:19:40 +0200

    refactor(components): separated blog page styling code from blog page implementation file

---
+++ components/Blog/styling/Blog.styles.ts
@@ -0,0 +1,61 @@
+import styled from 'styled-components';
+
+import {
+  LargeParagraph,
+  H2,
+  H3,
+  DefaultParagraph,
+} from 'components/atoms/typography.styles';
+
+export const TopDescription = styled(LargeParagraph)`
+  padding-bottom: 50px !important;
+  font-style: italic;
+
+  @media screen and (min-width: 768px) {
+    font-size: 36px;
+    line-height: 120%;
+    font-weight: 400;
+    padding-bottom: 55px !important;
+  }
+`;
+
+export const MainTitle = styled(H2)`
+  padding-bottom: 20px !important;
+
+  @media screen and (min-width: 768px) {
+    font-size: 48px;
+    line-height: 57.6px;
+    padding-bottom: 40px !important;
+  }
+`;
+
+export const MainContainer = styled.div`
+  padding: 30px 40px;
+  max-width: 860px;
+  margin: 0 auto;
+  
+  @media screen and (min-width: 768px) {
+    padding: 60px 40px 0;
+  }
+
+  ${H3},
+  ${LargeParagraph},
+  ${DefaultParagraph},
+  ${H2} {
+    padding-bottom: 30px;
+  }
+`;
+
+export const SocialShareOnlyOnDesktopWrapper = styled.div`
+  display: none;
+
+  @media screen and (min-width: 768px) {
+    display: block;
+  }
+`;
+
+export const BottomSocialShareWrapper = styled.div`
+  @media screen and (min-width: 768px) {
+    padding-top: 15px;
+  }
+`;
--- pages/blog.tsx
+++ pages/blog.tsx
@@ -1,12 +1,7 @@
-import styled from 'styled-components';
-
 import NavBar from 'components/NavBar/organisms/NavBar';
 import {
   Green010, Green100, Green050, Grey010,
 } from 'components/styling/colors';
-import {
-  H2, H3, LargeParagraph, DefaultParagraph,
-} from 'components/atoms/typography.styles';
 import SocialShare from 'components/Blog/organisms/SocialShare';
 import OtherArticles from 'components/Blog/organisms/OtherArticles';
 import TopSection from 'components/Blog/molecules/TopSection';
@@ -14,59 +9,13 @@ import DividingDots from 'components/Blog/molecules/DividingDots';
 import content from 'components/Blog/content/exampleArticle';
 import ArticleContent from 'components/Blog/templates/ArticleContent';
 import BackgroundWrapper from 'components/styling/BackgroundWrapper';
-
-const TopDescription = styled(LargeParagraph)`
-  padding-bottom: 50px !important;
-  font-style: italic;
-
-  @media screen and (min-width: 768px) {
-    font-size: 36px;
-    line-height: 120%;
-    font-weight: 400;
-    padding-bottom: 55px !important;
-  }
-`;
-
-const MainTitle = styled(H2)`
-  padding-bottom: 20px !important;
-
-  @media screen and (min-width: 768px) {
-    font-size: 48px;
-    line-height: 57.6px;
-    padding-bottom: 40px !important;
-  }
-`;
-
-const MainContainer = styled.div`
-  padding: 30px 40px;
-  max-width: 860px;
-  margin: 0 auto;
-  
-  @media screen and (min-width: 768px) {
-    padding: 60px 40px 0;
-  }
-
-  ${H3},
-  ${LargeParagraph},
-  ${DefaultParagraph},
-  ${H2} {
-    padding-bottom: 30px;
-  }
-`;
-
-const SocialShareOnlyOnDesktopWrapper = styled.div`
-  display: none;
-
-  @media screen and (min-width: 768px) {
-    display: block;
-  }
-`;
-
-const BottomSocialShareWrapper = styled.div`
-  @media screen and (min-width: 768px) {
-    padding-top: 15px;
-  }
-`;
+import {
+  MainContainer,
+  MainTitle,
+  TopDescription,
+  SocialShareOnlyOnDesktopWrapper,
+  BottomSocialShareWrapper,
+} from 'components/Blog/styling/Blog.styles';
 
 const Blog = () => (
   <>
