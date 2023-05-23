de66f710afc9b3c156c38af10d6f831c20ee6733
Balazs Radvanyi <balazs@riltech.co>
2023-05-23 16:25:15 +0200

    refactor(content): put content into a file mocking cms

---
+++ components/Prices/content/priceList.ts
@@ -0,0 +1,55 @@
+const priceListContent = [
+  {
+    categoryName: 'Rontás',
+    services: [
+      {
+        name: 'Rontás levétele',
+        price: '16.000.-Ft',
+      },
+      {
+        name: 'Rontás felvitele egy kiválasztott személyre',
+        price: '12.000.-Ft',
+      },
+      {
+        name: 'Rontás kiválasztása (tanácsadás) és felvitele',
+        price: '20.000.-Ft',
+      },
+      {
+        name: 'Rontás cseréje',
+        price: '25.000.-Ft',
+      },
+    ],
+  },
+  {
+    categoryName: 'Sötét varázslatok és kivédésük',
+    services: [
+      {
+        name: 'Avada kebabra!',
+        price: '24.000.-Ft',
+      },
+      {
+        name: 'Ekszörszájzbúk!',
+        price: '12.000.-Ft',
+      },
+    ],
+  },
+  {
+    categoryName: 'Ezotéria',
+    services: [
+      {
+        name: 'Ezokapu kinyitása',
+        price: '480.-Ft +ÁFA/perc',
+      },
+      {
+        name: 'Horoszkóp és aszcendens-tanácsadás',
+        price: '15.000.-Ft/alkalom (50 perc)',
+      },
+      {
+        name: 'Grabovoj-számsorok készítése (osztva Bősárkány és környéke)',
+        price: '5.000.-Ft/számsor',
+      },
+    ],
+  },
+];
+
+export default priceListContent;
