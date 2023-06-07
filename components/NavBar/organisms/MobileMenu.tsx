95a055cbbc67808bb62845798dc52005580f7ed8
Balazs Radvanyi <balazs@riltech.co>
2023-06-07 10:24:28 +0200

    wip: I don't know what the problem is

--- components/NavBar/organisms/MobileMenu.tsx
+++ components/NavBar/organisms/MobileMenu.tsx
@@ -1,17 +1,21 @@
 import styled from 'styled-components';
+import { IoChevronDownCircleOutline, IoChevronUpCircleOutline } from 'react-icons/io5';
+import AnimateHeight, { Height } from 'react-animate-height';
+import { useState } from 'react';
 
 import PsychoLink from 'components/atoms/PsychoLink';
-import { DefaultParagraph } from 'components/atoms/typography.styles';
-import { Green050, Grey010 } from 'components/styling/colors';
-import { menuItemsMobile } from 'components/NavBar/content/menuItems';
+import { DefaultParagraph, SmallParagraph } from 'components/atoms/typography.styles';
+import { Green010, Green050, Green100, Grey010 } from 'components/styling/colors';
+import { MenuItemWithSubItems, isLinkType, menuItemsMobile } from 'components/NavBar/content/menuItems';
 import { NavBarProps } from 'components/NavBar/organisms/NavBar';
 import ContactMenuButtonMobile from 'components/NavBar/molecules/ContactMenuButtonMobile';
+import { LinkType } from 'types/LinkType';
+import DividerLine from 'components/atoms/DividerLine';
 
 const MobileMenuContainer = styled.div`
     background-color: ${(props: NavBarProps) => props.backgroundColor ?? Grey010};
     padding: 30px 40px 40px;
     display: grid;
-    grid-template-rows: repeat(5, 1fr) 92px;
     row-gap: 20px;
     position: fixed;
     top: 83px;
@@ -20,32 +24,73 @@ const MobileMenuContainer = styled.div`
 
 const MobileMenuItem = styled(DefaultParagraph)`
     font-size: 20px;
-
+    
     @media screen and (min-width: 576px) {
       margin: auto;
     }
+    `;
+
+const SubItem = styled(DefaultParagraph)`
+  font-size: 20px;
+  padding-left: 8px; 
 `;
 
-const LinkWrapper = styled.span`
+const MenuItemWithSubItemsContainer = styled.div`
+  display: grid;
+  grid-template-columns: auto 28px;
+  justify-content: space-between;
 `;
+
+export const OpenToggle = styled.div`
+  cursor: pointer;
+`;
+
+// const LinkWrapper = styled.span`
+//   // needed because of the click handler
+// `;
 
 type MobileMenuProps = NavBarProps & {
   onMenuItemClicked: () => void;
 }
 
-const MobileMenu = ({ backgroundColor, onMenuItemClicked }: MobileMenuProps) => (
-  <MobileMenuContainer backgroundColor={backgroundColor}>
-    {menuItemsMobile.map((item, index) => (
-      <MobileMenuItem key={index} textColor={Green050}>
-        <LinkWrapper onClick={onMenuItemClicked}>
-          <PsychoLink href={item.linkHref}>
-            {item.linkText}
-          </PsychoLink>
-        </LinkWrapper>
-      </MobileMenuItem>
-    ))}
-    <ContactMenuButtonMobile onMenuItemClicked={onMenuItemClicked} />
-  </MobileMenuContainer>
-);
+const MobileMenu = ({ backgroundColor, onMenuItemClicked }: MobileMenuProps) => {
+  const [height, setHeight] = useState<Height>(0);
+  const toggleIsOpen = () => setHeight(height === 0 ? 'auto' : 0);
 
-export default MobileMenu;
+  return (
+    <MobileMenuContainer backgroundColor={backgroundColor}>
+      {menuItemsMobile.map((item, index) => {
+        if (isLinkType(item)) {
+          return (
+            <MobileMenuItem key={index} textColor={Green050} onClick={onMenuItemClicked}>
+              <PsychoLink href={(item as LinkType).linkHref}>
+                {(item as LinkType).linkText}
+              </PsychoLink>
+            </MobileMenuItem>
+          );
+        } else {
+            return (
+              <MenuItemWithSubItemsContainer>
+                <MobileMenuItem textColor={Green050}>
+                  {(item as MenuItemWithSubItems).name}
+                </MobileMenuItem>
+                <IoChevronDownCircleOutline />
+                <OpenToggle
+                  onClick={toggleIsOpen}
+                  onKeyPress={toggleIsOpen}
+                  role="button"
+                  tabIndex={0}
+                  aria-expanded={height !== 0}
+                  aria-controls="example-panel"
+                >
+                  {height !== 0
+                    ? <IoChevronUpCircleOutline size={28} />
+                    : <IoChevronDownCircleOutline size={28} />}
+                </OpenToggle>
+              <MenuItemWithSubItemsContainer />
+            ); 
+          }
+              <ContactMenuButtonMobile onMenuItemClicked={onMenuItemClicked} />
+            </MobileMenuContainer>
+})});
+      export default MobileMenu;
