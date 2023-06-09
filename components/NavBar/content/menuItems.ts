fbc4173d22ce2df4179070404079972f1cdd38ff
Balazs Radvanyi <balazs@riltech.co>
2023-06-09 11:14:24 +0200

    feat(mobile-menu): introduces submenu items

--- components/NavBar/content/menuItems.ts
+++ components/NavBar/content/menuItems.ts
@@ -1,5 +1,12 @@
 import { LinkType } from 'types/LinkType';
 
+export interface MenuItemWithSubItems {
+  name: string;
+  subItems: LinkType[];
+}
+
+export const isLinkType = (obj: any) => 'linkHref' in obj && 'linkText' in obj;
+
 export const menuItemsDesktop: LinkType[] = [
   { linkText: 'Magamról', linkHref: '/rolam' },
   { linkText: 'Amiben segíteni tudok', linkHref: '/' },
@@ -8,10 +15,32 @@ export const menuItemsDesktop: LinkType[] = [
   { linkText: 'GYIK', linkHref: '/gyik' },
 ];
 
-export const menuItemsMobile: LinkType[] = [
-  { linkText: 'Magamról', linkHref: '/rolam' },
-  { linkText: 'Amiben segíteni tudok', linkHref: '/' },
+export const menuItemsMobile: (LinkType | MenuItemWithSubItems)[] = [
+  {
+    name: 'Magamról',
+    subItems: [
+      { linkText: 'Életút', linkHref: '/rolam/eletut' },
+      { linkText: 'Képzések', linkHref: '/rolam/kepzesek' },
+      { linkText: 'Misszió', linkHref: '/rolam/misszió' },
+      { linkText: 'Publikációk és tagságok', linkHref: '/rolam/publikaciok-es-tagsagok' },
+    ],
+  },
+  {
+    name: 'Amiben segíteni tudok',
+    subItems: [
+      { linkText: 'Egyéni konzultáció', linkHref: '/amiben-segiteni-tudok/egyeni-konzultacio' },
+      { linkText: 'Csoportok', linkHref: '/amiben-segiteni-tudok/csoportok' },
+      { linkText: 'Művészetterápia', linkHref: '/amiben-segiteni-tudok/muveszetterapia' },
+      { linkText: 'Szervezetfejlesztés, tréning', linkHref: '/amiben-segiteni-tudok/szervezetfejlesztes-trening' },
+    ],
+  },
   { linkText: 'Árak', linkHref: '/arak' },
-  { linkText: 'Friss hírek', linkHref: '/blog' },
+  {
+    name: 'Friss hírek',
+    subItems: [
+      { linkText: 'Blog', linkHref: '/friss-hirek/blog' },
+      { linkText: 'Új események, csoportok', linkHref: '/friss-hirek/uj-esemenyek-es-csoportok' },
+    ],
+  },
   { linkText: 'Gyakran Ismételt Kérdések', linkHref: '/gyik' },
 ];
--- components/NavBar/organisms/MobileMenu.tsx
+++ components/NavBar/organisms/MobileMenu.tsx
@@ -1,17 +1,20 @@
 import styled from 'styled-components';
+import { IoChevronDownCircleOutline, IoChevronUpCircleOutline } from 'react-icons/io5';
+import AnimateHeight, { Height } from 'react-animate-height';
+import { useState } from 'react';
 
 import PsychoLink from 'components/atoms/PsychoLink';
 import { DefaultParagraph } from 'components/atoms/typography.styles';
-import { Green010, Green050, Grey010 } from 'components/styling/colors';
-import { menuItemsMobile } from 'components/NavBar/content/menuItems';
+import { Green010, Green050, Grey010, Green100 } from 'components/styling/colors';
+import { MenuItemWithSubItems, isLinkType, menuItemsMobile } from 'components/NavBar/content/menuItems';
 import { NavBarProps } from 'components/NavBar/organisms/NavBar';
 import ContactMenuButtonMobile from 'components/NavBar/molecules/ContactMenuButtonMobile';
+import { LinkType } from 'types/LinkType';
 
 const MobileMenuContainer = styled.div`
     background-color: ${(props: NavBarProps) => (props.colorScheme === 'light' ? Grey010 : Green010)};
     padding: 30px 40px 40px;
     display: grid;
-    grid-template-rows: repeat(5, 1fr) 92px;
     row-gap: 20px;
     position: fixed;
     top: 83px;
@@ -20,30 +23,108 @@ const MobileMenuContainer = styled.div`
 
 const MobileMenuItem = styled(DefaultParagraph)`
     font-size: 20px;
-
+    
     @media screen and (min-width: 576px) {
       margin: auto;
     }
+    `;
+
+const MenuItemWithSubItemsContainer = styled.div`
+  display: grid;
+  grid-template-columns: auto 28px;
+  justify-content: space-between;
 `;
 
-const LinkWrapper = styled.span`
+export const OpenToggle = styled.div`
+  cursor: pointer;
 `;
+
+// const LinkWrapper = styled.span`
+//   // needed because of the click handler
+// `;
 
 type MobileMenuProps = NavBarProps & {
   onMenuItemClicked: () => void;
 }
 
+interface MenuItemWithSubItemsProps {
+  item: MenuItemWithSubItems;
+  onMenuItemClicked: () => void;
+}
+
+const MenuItemsContainer = styled(AnimateHeight)`
+  div {
+    padding: 12px 0 12px 24px;
+    display: grid;
+    row-gap: 8px;
+  }
+`;
+
+const MenuItemWithSubItemsComponent = ({ item, onMenuItemClicked }: MenuItemWithSubItemsProps) => {
+  const [height, setHeight] = useState<Height>(0);
+  const toggleIsOpen = () => setHeight(height === 0 ? 'auto' : 0);
+
+  return (
+    <MenuItemWithSubItemsContainer>
+      <MobileMenuItem
+        textColor={Green050}
+        onClick={toggleIsOpen}
+        onKeyPress={toggleIsOpen}
+      >
+        {(item as MenuItemWithSubItems).name}
+      </MobileMenuItem>
+      <OpenToggle
+        onClick={toggleIsOpen}
+        onKeyPress={toggleIsOpen}
+        role="button"
+        tabIndex={0}
+        aria-expanded={height !== 0}
+        aria-controls="example-panel"
+      >
+        {height !== 0
+          ? <IoChevronUpCircleOutline size={28} color={Green100} />
+          : <IoChevronDownCircleOutline size={28} color={Green100} />}
+      </OpenToggle>
+      <MenuItemsContainer
+        duration={500}
+        height={height}
+      >
+        {item.subItems.map((subItem, subItemIndex) => (
+          <MobileMenuItem
+            key={subItemIndex}
+            textColor={Green050}
+            onClick={onMenuItemClicked}
+          >
+            <PsychoLink href={subItem.linkHref}>
+              {subItem.linkText}
+            </PsychoLink>
+          </MobileMenuItem>
+        ))}
+      </MenuItemsContainer>
+    </MenuItemWithSubItemsContainer>
+  );
+};
+
 const MobileMenu = ({ colorScheme, onMenuItemClicked }: MobileMenuProps) => (
   <MobileMenuContainer colorScheme={colorScheme}>
-    {menuItemsMobile.map((item, index) => (
-      <MobileMenuItem key={index} textColor={Green050}>
-        <LinkWrapper onClick={onMenuItemClicked}>
-          <PsychoLink href={item.linkHref}>
-            {item.linkText}
-          </PsychoLink>
-        </LinkWrapper>
-      </MobileMenuItem>
-    ))}
+    {menuItemsMobile.map((item, index) => {
+      if (isLinkType(item)) {
+        return (
+          <MobileMenuItem key={index} textColor={Green050} onClick={onMenuItemClicked}>
+            <PsychoLink href={(item as LinkType).linkHref}>
+              {(item as LinkType).linkText}
+            </PsychoLink>
+          </MobileMenuItem>
+        );
+      }
+
+      return (
+        <MenuItemWithSubItemsComponent
+          item={item as MenuItemWithSubItems}
+          onMenuItemClicked={onMenuItemClicked}
+        />
+      );
+    })}
     <ContactMenuButtonMobile onMenuItemClicked={onMenuItemClicked} />
   </MobileMenuContainer>
 );
